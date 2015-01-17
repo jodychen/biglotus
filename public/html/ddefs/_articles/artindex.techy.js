7a6646cda4fc0adba584fe0491dc796c9218e45f
Andy Newcomb <yzandy@users.noreply.github.com>
2015-01-16 21:32:49 -0500

    Update artindex.techy.js

--- public/html/ddefs/_articles/artindex.techy.js
+++ public/html/ddefs/_articles/artindex.techy.js
@@ -1,9 +1,15 @@
 module.exports = function() {
     var artindex = '',
     pages = this.pages();
-
+    artindex += '<script> var ddefs = [';
+    var ppa = [];
+    var p;
     for(var i=0; i < pages.length, page=pages[i]; i++) {
-	artindex += '<a href="' + page.get('paths').url + '">' + page.get('title') + '</a>\n';
+	// artindex += '<a href="' + page.get('paths').url + '">' + page.get('title') + '</a>\n';
+	p = '{  Title: "' +  page.get('title') + "', Url: "' + page.get('paths').url + '" }'; 
+	ppa.push(p);
     }
+    artindex += ppa.join(",\n")
+    artindex += "\n]</script>";
     return artindex;
 }
