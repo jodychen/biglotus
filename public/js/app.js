f29507e9919b78d208dcbec6cb4e7a399b54a81a
Andy Newcomb <anewcomb@Andys-MacBook-Air.local>
2015-01-21 11:04:50 -0500

    reverting away from compileProvider.debugInfoEnabled(false)

--- public/js/app.js
+++ public/js/app.js
@@ -5,11 +5,10 @@
 
 /*
 var ddefs = [{  Title: "Title ABC", Url: "/abc.html" },
-	     {  Title: "Title One", Url: "/one.html" },
-	     {  Title: "Title Page", Url: "/page.html" },
-	     {  Title: "Title Two", Url: "/two.html" }
-	     ];
-
+     {  Title: "Title One", Url: "/one.html" },
+          {  Title: "Title Page", Url: "/page.html" },
+	       {  Title: "Title Two", Url: "/two.html" }
+	            ];
 */
 
 var myApp = angular.module('myApp',  ['ngRoute', 'ui.bootstrap', 'uiGmapgoogle-maps', 'ngSanitize']);
@@ -18,56 +17,49 @@ myApp
     .config(function($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl: 'html/login.html',
-                controller: 'authentication'
-            })
+		    templateUrl: 'html/login.html',
+			controller: 'authentication'
+			})
             .when('/map', {
-                templateUrl: 'html/map.html',
-                // controller: 'showMap'
-                // controller: 'zipCodeCtrl'
-                controller: 'geoCtrl'
-            })
+		    templateUrl: 'html/map.html',
+			// controller: 'showMap'
+			// controller: 'zipCodeCtrl'
+			controller: 'geoCtrl'
+			})
             .when('/dharmadictionary/:defId', {
-                templateUrl: 'html/detail-ddef.html',
-                controller: 'ddefCtrl'
-            })
+		    templateUrl: 'html/detail-ddef.html',
+			controller: 'ddefCtrl'
+			})
             .otherwise({
-                redirectTo: '/'
-            });
+		    redirectTo: '/'
+			});
         console.log('end of config function');
         })
     .config(function(uiGmapGoogleMapApiProvider) {
-        uiGmapGoogleMapApiProvider.configure({
-            key: 'AIzaSyCfKe-nvx2gcpQDBlTjLXCQ8TerMHHV0Vw',
-            v: '3.17',
-            libraries: 'weather,geometry,visualization'
-        });
-    })
-    .config(['$compileProvider', function ($compileProvider) {
-	$compileProvider.debugInfoEnabled(false);
-    }])  
-    /*  If you wish to debug an application with this information then 
-     * you should open up a debug console in the browser then call this method directly in this console:
-     * angular.reloadWithDebugInfo(); 
-     */
+	    uiGmapGoogleMapApiProvider.configure({
+		    key: 'AIzaSyCfKe-nvx2gcpQDBlTjLXCQ8TerMHHV0Vw',
+			v: '3.17',
+			libraries: 'weather,geometry,visualization'
+			});
+	})
     .controller('navCtrl', function ($scope, $log) {
-        $log.log('within nav function');
-        $scope.isCollapsed = true;
-    })
+	    $log.log('within nav function');
+	    $scope.isCollapsed = true;
+	})
     .controller('authentication', function ($scope, $routeParams) {
-        $scope.params = $routeParams;
-    })
+	    $scope.params = $routeParams;
+	})
     .controller('ddefCtrl', function ($scope, $routeParams, $http) {
 	    var ddefs;
 	    $scope.params = $routeParams;
 	    $http.get('/html/ddefs/_articles/_dist/abc.json').success (function(data){
-		      console.log( 'data = ' + data);
+		    console.log( 'data = ' + data);
 		    ddefs = angular.fromJson(data);
 		    $scope.ddefs = ddefs;
 		    $http.get('/html/ddefs/_articles/_dist/' + ddefs[$scope.params.defId].Url).success (function(data){
 			    $scope.myHTML = data;
+			});
 		});
-	    });
 	})
 
     .controller('ExampleController', [
@@ -91,73 +83,73 @@ myApp
 
 
     .controller('geoCtrl', [
-        '$scope', 
-        'GeolocationService', 
-				      'uiGmapIsReady',
-        function ($scope, geolocation) {
-            $scope.position = null;
-            $scope.message = "Determining geolocation...";
+			    '$scope', 
+			    'GeolocationService', 
+			    'uiGmapIsReady',
+			    function ($scope, geolocation) {
+				$scope.position = null;
+				$scope.message = "Determining geolocation...";
 
-            geolocation().then(
-			       function (position, IsReady) {
-                    $scope.position = position;
-                    $scope.map = { center: { latitude: $scope.position.coords.latitude, longitude: $scope.position.coords.longitude }, zoom: 13 };
-                    $scope.marker1 = {
-                        id: 1,
-                        coords: {
-                            latitude: 40.742683,
-                            longitude: -73.873578
-                        },
-                        options: {title:'Chan Meditation Center', labelContent : 'Chan Meditation Center of Dharma Drum', labelClass: 'nav-marker'}
-                    }
-                    $scope.marker2 = {
-                        id: 2,
-                        coords: {
-                            latitude: $scope.position.coords.latitude, longitude: $scope.position.coords.longitude
-                        },
-                        options: {title:'You Are Here', labelContent : 'This is Where You Are', labelClass: 'nav-marker' }
-                    }
-                }, 
-                function (reason) {
+				geolocation().then(
+						   function (position, IsReady) {
+						       $scope.position = position;
+						       $scope.map = { center: { latitude: $scope.position.coords.latitude, longitude: $scope.position.coords.longitude }, zoom: 13 };
+						       $scope.marker1 = {
+							   id: 1,
+							   coords: {
+							       latitude: 40.742683,
+							       longitude: -73.873578
+							   },
+							   options: {title:'Chan Meditation Center', labelContent : 'Chan Meditation Center of Dharma Drum', labelClass: 'nav-marker'}
+						       }
+						       $scope.marker2 = {
+							   id: 2,
+							   coords: {
+							       latitude: $scope.position.coords.latitude, longitude: $scope.position.coords.longitude
+							   },
+							   options: {title:'You Are Here', labelContent : 'This is Where You Are', labelClass: 'nav-marker' }
+						       }
+						   }, 
+						   function (reason) {
                     $scope.message = "Could not be determined."
-                }
-            );
-        }
+			}
+						   );
+			    }
 
-    ]);
+			    ]);
     
     
-    myApp.factory('GeolocationService', [
-        '$q', 
-        '$window', 
-        '$rootScope',
-        function($q, $window, $rootScope) {
-            return function() {
-                var deferred = $q.defer();
+myApp.factory('GeolocationService', [
+				     '$q', 
+				     '$window', 
+				     '$rootScope',
+				     function($q, $window, $rootScope) {
+					 return function() {
+					     var deferred = $q.defer();
 
-                if(!$window.navigator) {
-                    $rootScope.$apply(function() {
-                        deferred.reject(new Error('Geolocation is not supported'));
-                    });
-                } 
-                else {
-                    $window.navigator.geolocation.getCurrentPosition(function(position) {
-                        $rootScope.$apply(function() {
-                                deferred.resolve(position);
-                                // deferred.resolve({ lat: position.coords.latitude, lng: position.coords.longitude });
-                        });
-                    }, function(error) {
-                        $rootScope.$apply(function() {
-                            deferred.reject(error);
-                        });
-                    });
-                }
-                return deferred.promise;
-            };
-        }
+					     if(!$window.navigator) {
+						 $rootScope.$apply(function() {
+							 deferred.reject(new Error('Geolocation is not supported'));
+						     });
+					     } 
+					     else {
+						 $window.navigator.geolocation.getCurrentPosition(function(position) {
+							 $rootScope.$apply(function() {
+								 deferred.resolve(position);
+								 // deferred.resolve({ lat: position.coords.latitude, lng: position.coords.longitude });
+							     });
+						     }, function(error) {
+							 $rootScope.$apply(function() {
+								 deferred.reject(error);
+							     });
+						     });
+					     }
+					     return deferred.promise;
+					 };
+				     }
 
 
-    ]);
+				     ]);
 
 
 //  CMC:  40.742683, -73.873578
